f016f8c1ad9c68dd5baa1da47f141bd8da07135d
koson <567256+koson@users.noreply.github.com>
2024-03-11 01:01:19 +0700

    add exercise

---
+++ Lab11_Ex04/Program.cs
@@ -0,0 +1,29 @@
+﻿Base b = new Base();
+Derived_1 d1 = new Derived_1();
+Derived_2 d2 = new Derived_2();
+
+b.A();
+d1.A();
+d2.A();
+
+class Base
+{
+    public virtual void A()
+    {
+        System.Console.WriteLine("Base.A()");
+    }
+}
+class Derived_1 : Base
+{
+    public override void A()
+    {
+        System.Console.WriteLine("Derived_1.A()");
+    }
+}
+class Derived_2 : Derived_1
+{
+    public override void A()
+    {
+        System.Console.WriteLine("Derived_2.A()");
+    }
+}
